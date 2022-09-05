bd3865b3cc1b1a0e8f0a34947291eaed6982445c
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-05 17:07:18 +0300

    add Task 005 Integer Numbers

---
+++ Task_005_Integer_Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondNumber = -number - 1;
+
+while (secondNumber < number)
+{
+    secondNumber ++;
+    Console.WriteLine(secondNumber);
+}
