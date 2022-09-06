202f367f8e6dc961ba3112fd95a8deeaec0e4725
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-06 18:04:42 +0300

    add task 014 multiple 7 and 23

---
+++ Task_014_Sem2/Program.cs
@@ -0,0 +1,25 @@
+﻿// 14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Multiple(int num)
+{
+    if (num % 7 == 0)
+    {
+        if (num % 23 == 0)
+        {
+            Console.WriteLine("да");
+        }
+         else
+        {
+            Console.WriteLine("нет");
+        }
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+
+Multiple(number);
