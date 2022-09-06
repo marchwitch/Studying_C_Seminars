cc090303b5585b1317d6e0897432cde60a565b01
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-06 16:13:48 +0300

    add task 009 Find max (method)

---
+++ Task_009_Sem2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int randomNum = new Random().Next(1, 100);
+Console.WriteLine(randomNum);
+
+int digitFirst = randomNum / 10;
+int digitSecond = randomNum % 10;
+
+Console.WriteLine(FindMax(digitFirst, digitSecond));
+
+int FindMax(int num1, int num2)
+{
+    if(num1 > num2)
+    {
+        return num1;
+    }
+    return num2;
+}
