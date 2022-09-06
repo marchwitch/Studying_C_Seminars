74c4fb5dff1769c10db09ffd370e8aeb7ceeb598
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-06 18:01:48 +0300

    add task 011 delete second number

---
+++ Task_011_Sem2/Program.cs
@@ -0,0 +1,22 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int randomNum = new Random().Next(100, 1000);
+Console.WriteLine(randomNum);
+
+// int digit1 = randomNum / 100;
+// int digit2 = randomNum % 10;
+// Console.WriteLine(digit1);
+// Console.WriteLine(digit2);
+// int digit3 = digit1*10 + digit2;
+// Console.WriteLine(digit3);
+
+int DeleteSecondNumber(int threeDigitNum)
+{
+    int digit1 = threeDigitNum / 100;
+    int digit2 = threeDigitNum % 10;
+    int digit3 = digit1*10 + digit2;
+    return digit3;
+}
+
+int newNumber = DeleteSecondNumber(randomNum);
+Console.WriteLine(newNumber);
