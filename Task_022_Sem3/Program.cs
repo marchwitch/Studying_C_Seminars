cd2ed6e24b7b21d8297156413c37de29b53b941a
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-07 11:46:17 +0300

    add task 022 for sem3 (square of numbers(1 to N)

---
+++ Task_022_Sem3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Enter N:");
+int N = Convert.ToInt32(Console.ReadLine());
+GetDashboard(N);
+
+void GetDashboard(int num)
+{
+    int count = 1;
+    while(count <= Math.Abs(num))
+    {
+        Console.WriteLine($"Квадрат числа {count} = {count*count}");
+        count++;
+    }
+}
