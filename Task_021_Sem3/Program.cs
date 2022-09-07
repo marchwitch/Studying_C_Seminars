356709f191156c7efe2172c0bfacb2d413df6c7f
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-07 11:44:49 +0300

    add task 021 for sem3 (distance between points)

---
+++ Task_021_Sem3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+Console.WriteLine("Enter x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double GetDistance(int x1, int y1, int x2, int y2)
+{
+    double distance = 0;
+    distance = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2));
+    return distance;
+}
+
+Console.WriteLine(GetDistance(x1, y1, x2, y2));
