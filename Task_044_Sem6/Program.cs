a017c1abf824751ed74e11042a3629423e28b6a9
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-29 13:18:57 +0300

    added task 44 for sem6

---
+++ Task_044_Sem6/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3; Если N = 3 -> 0 1 1; Если N = 7 -> 0 1 1 2 3 5 8
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+void GetNumbersFibonacci(int num)
+{
+    int firstNum = 0;
+    int secondNum = 1;
+    Console.WriteLine(firstNum);
+    Console.WriteLine(secondNum);
+
+    for (int i = 3; i <= num; i++)
+    {
+        Console.WriteLine(secondNum + firstNum);
+        int tmp = secondNum;
+        secondNum = firstNum + secondNum;
+        firstNum = tmp;
+    }
+}
+
+int number = Prompt("Введите число: ");
+GetNumbersFibonacci(number);
