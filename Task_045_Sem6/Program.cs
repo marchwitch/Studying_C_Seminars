b7b86b7e294509cd89d773a736b68e5b306dac64
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-29 13:19:21 +0300

    added task 45 for sem6

---
+++ Task_045_Sem6/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] CopyArray(int[] collection)
+{
+    int[] copy = new int[collection.Length];
+    for (int i = 0; i < collection.Length; i++)
+    {
+        copy[i] = collection[i];
+    }
+    return copy;
+}
+
+void PrintArray(int[] coll)
+{
+    int length = coll.Length; 
+    int index = 0; 
+    while(index < length)
+    {
+        Console.Write($"{coll[index]} ");  
+        index++;      
+    }
+}
+
+int[] array = {1,2,3,4,5};
+var tmp = CopyArray(array);
+PrintArray(tmp);
