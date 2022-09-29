340c9dd7b40dc6509a0f6c36e1b352470969a25b
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-29 13:21:19 +0300

    added task 51 for sem7

---
+++ Task_051_Sem7/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumGeneralDiagonal(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i == j)
+            {
+                sum = sum + matrix[i,j];
+            }
+        }
+    }
+    return sum;
+}
+
+int rows = new Random().Next(3, 6);
+int columns = new Random().Next(3, 6);
+int[,] array = new int[rows,columns];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(SumGeneralDiagonal(array));
