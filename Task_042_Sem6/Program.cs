557f5cd9158fe5650d86d63bbc539a71727ab074
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-29 13:18:34 +0300

    added task 42 for sem6

---
+++ Task_042_Sem6/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101; 3  -> 11; 2  -> 10
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+string GetNumber(int num)
+{
+    string result = "";
+    while(num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2;
+    }
+    return result;
+}
+
+int number = Prompt("Введите число: ");
+Console.WriteLine(GetNumber(number));
