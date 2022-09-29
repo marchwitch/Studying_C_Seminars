20858f4b7a9f26362ec7dec755dea1fcb2234aad
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-29 13:20:52 +0300

    added task 49 for sem7

---
+++ Task_049_Sem7/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReplaceBySquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i,j] *= matrix[i,j];
+            }
+        }
+    }
+}
+
+
+int rows = new Random().Next(3, 6);
+int columns = new Random().Next(3, 6);
+int[,] array = new int[rows,columns];
+
+FillArray(array);
+PrintArray(array);
+ReplaceBySquare(array);
+Console.WriteLine();
+PrintArray(array);
