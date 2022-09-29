eba6dd0a9e536bc01a467ee24d5e06cbd88c8cd5
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-29 13:17:42 +0300

    added task 39 for sem6

---
+++ Task_039_Sem6/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+//[1 2 3 4 5] -> [5 4 3 2 1]; [6 7 3 6] -> [6 3 7 6]
+
+void Reverse(int[] array)
+{
+    for (int i = 0, j = array.Length - 1; i < array.Length / 2; i++, j--)
+    {
+       int tmp = array[i];
+       array[i] = array[j];
+       array[j] = tmp;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item}\t");
+    }
+}
+
+int[] arr = new int[] {6,5,8,9,6,3};
+Reverse(arr);
+PrintArray(arr);
