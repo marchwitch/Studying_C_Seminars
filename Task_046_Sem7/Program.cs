fa61d3348a64ea995ccb7ab2638772e4fc27a623
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-29 13:19:54 +0300

    added task 46 for sem7

---
+++ Task_046_Sem7/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+int rows = new Random().Next(3, 6);
+int columns = new Random().Next(3, 6);
+int[,] array = new int[rows, columns];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(1, 10);
+        Console.Write($"{array[i,j]}");
+    }
+    Console.WriteLine();
+}
