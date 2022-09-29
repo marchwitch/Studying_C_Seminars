8988cccf0ef68242805b94edcecb47c57f635444
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-29 13:11:41 +0300

    added task 2 for sem4

---
+++ Task_002_Sem4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3; 78 -> 2; 89126 -> 5
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int CountDigit (int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
+
+int num = Prompt("Введите число > ");
+int countOfDigits = CountDigit(num);
+Console.WriteLine($"Количество цифр в числе равно {countOfDigits}");
