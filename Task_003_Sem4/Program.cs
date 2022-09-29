75a988a647d3dbf952df9ccd23d6c14a10b8ce33
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-29 13:12:13 +0300

    added task 3 for sem4

---
+++ Task_003_Sem4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24; 5 -> 120
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+// int MultiplicationNumbers(int number)
+// {
+//     int multiplication = 1;
+//     for (int i = 1; i <= number; i++)
+//     {
+//         multiplication = multiplication*i;
+//     }
+//     return multiplication;
+// }
+
+int FindFact(int number)
+{
+    int fact = 1;
+    while (number > 0)
+    {
+        fact *= number;
+        number = number - 1;
+    }
+    return fact;
+}
+
+
+int num = Prompt("Введите число > ");
+// int multipl = MultiplicationNumbers(num);
+Console.WriteLine($"Произведение чисел равно {FindFact(num)}");
